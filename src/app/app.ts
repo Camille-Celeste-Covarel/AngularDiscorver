91daa9c00e43f347c628e950e8844010047abda7
Covarel <covarelcamille@gmail.com>
2025-10-03 20:02:47 +0200

    add biome

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: "app-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./normalize.css",
 })
 export class App {
-  protected readonly title = signal('discover-angular');
+	protected readonly title = signal("discover-angular");
 }
